f959d9385db0d10a2e9dd456914c0d4b4a4aee09
kunal gaurav <gauravkunalms@gmail.com>
2023-08-11 20:00:11 +0530

    this is my first commit

---
+++ src/academy.js
@@ -0,0 +1,15 @@
+function validateForm() {
+    alert("hello")
+    var username = document.forms["loginForm"]["username"].value;
+    var password = document.forms["loginForm"]["password"].value;
+
+    if (username === "") {
+        alert("Username must be filled out");
+        return false;
+    }
+
+    if (password === "") {
+        alert("Password must be filled out");
+        return false;
+    }
+}
